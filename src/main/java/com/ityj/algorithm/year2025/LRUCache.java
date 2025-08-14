ea1b2e33238c28e31c38486dbc14cc35ed7bd017
Jack <ayinjun1109@163.com>
2025-08-14 11:38:58 +0800

    test 0814

--- src/main/java/com/ityj/algorithm/year2025/LRUCache.java
+++ src/main/java/com/ityj/algorithm/year2025/LRUCache.java
@@ -1,18 +1,110 @@
 package com.ityj.algorithm.year2025;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Node {
+    int key;
+    int value;
+    Node pre;
+    Node next;
+    public Node() {
+        this.key = 0;
+        this.value = 0;
+        pre = null;
+        next = null;
+    }
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        pre = null;
+        next = null;
+    }
+}
+
 class LRUCache {
-/*
+    private Node head;
+    private Node tail;
+    private Map<Integer, Node> map;
+    int capacity;
+    int size;
+
+
     public LRUCache(int capacity) {
-        
+        this.capacity = capacity;
+        head = new Node();
+        tail = new Node();
+        map = new HashMap<>();
+        head.next = tail;
+        tail.pre = head;
     }
     
     public int get(int key) {
-        
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            removeNode(node);
+            addNodeToHead(node);
+            return node.value;
+        } else {
+            return -1;
+        }
     }
     
     public void put(int key, int value) {
-        
-    }*/
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            removeNode(node);
+            map.remove(key);
+            size--;
+        }
+        if (size == capacity) {
+            Node toRemove = tail.pre;
+            removeNode(toRemove);
+            map.remove(toRemove.key);
+            size--;
+        }
+        Node node = new Node(key, value);
+        addNodeToHead(node);
+        map.put(key, node);
+        size++;
+    }
+
+    public void removeNode(Node node) {
+        Node pre = node.pre;
+        Node next = node.next;
+        pre.next = next;
+        next.pre = pre;
+    }
+
+    public void addNodeToHead(Node node) {
+        Node tmp = head.next;
+        node.pre = head;
+        head.next = node;
+        node.next = tmp;
+        tmp.pre = node;
+    }
+
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        int i = cache.get(1);
+        cache.put(3, 3);
+        int i2 = cache.get(2);
+        cache.put(4, 4);
+        int i3 = cache.get(1);
+        int i4 = cache.get(3);
+        int i5 = cache.get(4);
+        System.out.println("i5 = " + i);
+        System.out.println("i5 = " + i2);
+        System.out.println("i5 = " + i3);
+        System.out.println("i5 = " + i4);
+        System.out.println("i5 = " + i5);
+    }
+
 }
 
 /**
--- src/main/java/com/ityj/algorithm/year2025/TestAugust.java
+++ src/main/java/com/ityj/algorithm/year2025/TestAugust.java
@@ -1,5 +1,6 @@
 package com.ityj.algorithm.year2025;
 
+import com.ityj.algorithm.entity.ListNode;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -63,8 +64,150 @@ public class TestAugust {
         return result;
     }
 
+    // 输入：nums = [1,2,3], k = 3
+    //输出：2
+    public int subarraySum_test14(int[] nums, int k) {
+        if (nums == null) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+            if (sum == k) {
+                result++;
+            }
+            int j = i + 1;
+            while (true) {
+                if (j >= nums.length) {
+                    break;
+                }
+                sum += nums[j];
+                if (sum == k) {
+                    result++;
+                }
+                j++;
+            }
+        }
+        return result;
+    }
 
-    @Test
+    // 输入：head = [1,2,3,4,5]
+    //输出：[5,4,3,2,1]
+    public ListNode reverseList_test14(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode pre = null;
+        while (true) {
+            if (head == null) {
+                break;
+            }
+            ListNode tmp = head.next;
+            head.next = pre;
+            pre = head;
+            head = tmp;
+        }
+        return pre;
+    }
+
+
+    private ListNode front = new ListNode(-1);
+    public boolean isPalindrome_test14(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        front.next = head;
+        return check(head);
+
+    }
+
+    private boolean check(ListNode head) {
+        if (head != null) {
+            if (!check(head.next)) {
+                return false;
+            }
+            if (head.val != front.next.val) {
+                return false;
+            }
+            front = front.next;
+        }
+        return true;
+    }
+
+    // 输入：head = [1,2,3,4]
+    //输出：[2,1,4,3]
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (true) {
+            if (pre.next == null || pre.next.next == null) {
+                break;
+            }
+            ListNode tmp = pre.next;
+            ListNode tmp2 = pre.next.next.next;
+            pre.next = pre.next.next;
+            pre.next.next = tmp;
+            tmp.next = tmp2;
+            pre = tmp;
+        }
+        return dummy.next;
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> tmpList = new ArrayList<>();
+        backTracking_0814(candidates, target, result, tmpList, 0, 0);
+        return result;
+    }
+
+    private void backTracking_0814(int[] candidates, int target, List<List<Integer>> result,
+                                   List<Integer> tmpList, int sum, int fromIndex) {
+        if (sum >= target) {
+            if (sum == target) {
+                result.add(new ArrayList<>(tmpList));
+            }
+            return;
+        }
+
+        for (int i = fromIndex; i < candidates.length; i++) {
+            tmpList.add(candidates[i]);
+            backTracking_0814(candidates, target, result, tmpList, sum + candidates[i], i);
+            tmpList.remove(tmpList.size() - 1);
+        }
+    }
+
+    // 输入: nums = [1,3,5,6], target = 5
+    //输出: 2
+    public int searchInsert(int[] nums, int target) {
+
+        if (nums == null) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (true) {
+            if (left >= right) {
+                break;
+            }
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return left;
+    }
+
+
+        @Test
     public void test1() {
         int[] nums = {-4,-1,-1,0,1,2};
         List<List<Integer>> lists = threeSum_test13(nums);
